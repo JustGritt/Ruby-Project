50235b08bc012a51993b58f53a03723efc928682
JustGritt <alexistan9712@gmail.com>
2022-06-25 15:55:19 +0200

    ❤ Add Posts & Contact routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "contact", to: "contact#index"
+
+
+  namespace :admin do
+    resources :posts
+
+    root to: "posts#index"
+  end
+  resources :posts
+
 end
